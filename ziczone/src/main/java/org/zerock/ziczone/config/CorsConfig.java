2ffcf4758193a9d3175a2996b8d5461a88cf0a45
Gaannini <0107162a@gmail.com>
2024-10-11 04:59:27 +0900

    refactor: CORS 설정을 SecurityConfig에서 일괄 관리하도록 변경

--- ziczone/src/main/java/org/zerock/ziczone/config/CorsConfig.java
+++ ziczone/src/main/java/org/zerock/ziczone/config/CorsConfig.java
@@ -1,19 +1,19 @@
-package org.zerock.ziczone.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // 허용할 출처
-                .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-}
+//package org.zerock.ziczone.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("http://localhost:3000") // 허용할 출처
+//                .allowedMethods("*")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//}
